fbef5dccc203c49a3425821a9d10c22d1d433caf
Sho0226 <s1F102201616@iniad.org>
2024-06-25 01:50:07 +0900

    fix: othello code delete

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,44 +26,8 @@ const Home = () => {
     [2, 3, 4, 5, 6, 4, 3, 2],
   ]);
 
-  const directions = [
-    [0, 1], // Down
-    [1, 1], // Down-right
-    [1, 0], // Right
-    [1, -1], // Up-right
-    [0, -1], // Up
-    [-1, -1], // Up-left
-    [-1, 0], // Left
-    [-1, 1], // Down-left
-  ];
-
   const newBoard = structuredClone(board);
-  const clickHandler = (x: number, y: number) => {
-    if (board[y][x] !== 0) return;
-    for (const direct of directions) {
-      const [x_d, y_d] = direct;
-
-      for (let i = 1; i < 8; i++) {
-        if (
-          board[y + y_d * i] !== undefined &&
-          board[y + y_d * i][x + x_d * i] !== undefined &&
-          board[y + y_d][x + x_d] !== 0 &&
-          board[y + y_d][x + x_d] === 3 - turnColor
-        ) {
-          if (board[y + y_d * i][x + x_d * i] === turnColor) {
-            Array.from({ length: i + 1 }, (_, s) => {
-              newBoard[y + y_d * s][x + x_d * s] = turnColor;
-            });
-            newBoard[y + y_d * i][x + x_d * i] = turnColor;
-            setTurnColor(3 - turnColor);
-            setBoard(newBoard);
-
-            break;
-          }
-        }
-      }
-    }
-  };
+  const clickHandler = (x: number, y: number) => {};
 
   const getImageSrc = (color: number) => {
     switch (color) {
@@ -79,8 +43,6 @@ const Home = () => {
         return turnColor === 1 ? fiveWhite : fiveBlack;
       case 6:
         return turnColor === 1 ? sixWhite : sixBlack;
-      default:
-        return '';
     }
   };
 
@@ -95,9 +57,7 @@ const Home = () => {
             };
             let content = null;
             if (color !== 0) {
-              content = (
-                <img className={styles.imgstyle} src={getImageSrc(color).src} alt="1b.png" />
-              );
+              content = <img className={styles.imgstyle} src={getImageSrc(color)?.src} />;
             }
 
             return (
