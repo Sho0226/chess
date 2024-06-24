f5f53640c4cc78400da0f5717d202cdf919a8268
Sho0226 <s1F102201616@iniad.org>
2024-06-24 15:29:08 +0900

    add: board参照

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,29 @@
 import styles from './index.module.css';
 import { useState } from 'react';
-import SampleImage from '../assets/images/1b.png';
+import oneBlack from '../assets/images/1b.png';
+import oneWhite from '../assets/images/1w.png';
+import twoBlack from '../assets/images/2b.png';
+import twoWhite from '../assets/images/2w.png';
+import threeBlack from '../assets/images/3b.png';
+import threeWhite from '../assets/images/3w.png';
+import fourBlack from '../assets/images/4b.png';
+import fourWhite from '../assets/images/4w.png';
+import fiveBlack from '../assets/images/5b.png';
+import fiveWhite from '../assets/images/5w.png';
+import sixBlack from '../assets/images/6b.png';
+import sixWhite from '../assets/images/6w.png';
 
 const Home = () => {
   const [turnColor, setTurnColor] = useState(1);
   const [board, setBoard] = useState([
+    [2, 3, 4, 6, 5, 4, 3, 2],
+    [1, 1, 1, 1, 1, 1, 1, 1],
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 1, 2, 0, 0, 0],
-    [0, 0, 0, 2, 1, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 4, 3, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 1, 1, 1, 1, 1, 1, 1],
+    [2, 3, 4, 5, 6, 4, 3, 2],
   ]);
 
   const directions = [
@@ -84,7 +95,9 @@ const Home = () => {
             };
             let content = null;
             if (color !== 0) {
-              content = <img className={styles.imgstyle} src={SampleImage.src} alt="1b.png" />;
+              content = (
+                <img className={styles.imgstyle} src={getImageSrc(color).src} alt="1b.png" />
+              );
             }
 
             return (
